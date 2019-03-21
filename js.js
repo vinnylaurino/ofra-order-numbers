7b7e7f3bb8a21d116decc4562676de7b52862dd6
vinnylaurino <vinny.laurino@gmail.com>
2019-03-21 07:38:04 -0400

    added scanning on pageload

--- js.js
+++ js.js
@@ -1,4 +1,3 @@
-
 var jsonUrl = "https://api.airtable.com/v0/app85WLPCysvMb4vj/orders?api_key=key1puFSUx6VTe5pj";
 var Airtable = require('airtable');
 var base = new Airtable({
@@ -11,42 +10,47 @@ var day = d.getDate();
 var currentDay = d.getFullYear() + '-' +
     (('' + month).length < 2 ? '0' : '') + month + '-' +
     (('' + day).length < 2 ? '0' : '') + day;
-    var currentDayFormatted =
-        (('' + month).length < 2 ? '0' : '') + month + '-' +
-        (('' + day).length < 2 ? '0' : '') + day;
+var currentDayFormatted =
+    (('' + month).length < 2 ? '0' : '') + month + '-' +
+    (('' + day).length < 2 ? '0' : '') + day;
 var orderNum = $("#newOrderNumber").val();
 var currentTime = '';
 var rowItem = '';
 var duplicate = false;
+var allOrders = base('Orders').select({
+    maxRecords: 2000,
+    view: 'Grid view'
+});
 
-
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
     if ($('#newOrderNumber').val() != '') {
         event.preventDefault();
-                  function addZero(i) {
+
+        function addZero(i) {
             if (i < 10) {
-              i = "0" + i;
+                i = "0" + i;
             }
             return i;
-          };
+        }
         var orderNum = $("#newOrderNumber").val();
         var currentDate = new Date();
         var currentTime = `${addZero(currentDate.getHours())}:${addZero(currentDate.getMinutes())}  ${currentDayFormatted}`;
 
-        checkDuplicates(orderNum, currentTime, duplicate); // CREATES THE ORDER BUT CHECKS FOR DUPES FIRST
+        checkDuplicates(orderNum, currentTime, duplicate); // CHECKS FOR DUPES IF FOUND CREATES ORDER
+        
     }
-    setTimeout(function() {
+    setTimeout(function () {
         $("#newOrderNumber").focus().val('');
     }, 250);
 });
 
 function registerOrder(orderNum, currentTime) { // FUNCTION TO CREATE THE ORDER
-    var rowItem = '<div class="item-row flex-row row"><div data-order-number="' + orderNum + '" class="order-number fifty">' + orderNum + '</div><div data-order-time="' + currentTime + '" class="order-time fifty">' + currentTime + '</div>'
+    var rowItem = '<div class="item-row flex-row row"><div data-order-number="' + orderNum + '" class="order-number fifty">' + orderNum + '</div><div data-order-time="' + currentTime + '" class="order-time fifty">' + currentTime + '</div>';
     base('Orders').create({
             "OrderNum": orderNum,
             "OrderDate": currentTime
         },
-        function(err, record) {
+        function (err, record) {
             if (err) {
                 console.error(err);
                 return;
@@ -55,76 +59,78 @@ function registerOrder(orderNum, currentTime) { // FUNCTION TO CREATE THE ORDER
             goToBottom();
             //console.log(rowItem);
             //console.log(record.id);
-        })
+        });
 }
+
 
 function checkDuplicates(orderNum, currentTime, duplicate) { // FUNCTION TO CHECK FOR DUPLICATES
-    return $.ajax({
-        type: "GET",
-        url: jsonUrl,
-        success: function findDupe(data, duplicate) {
-            function compareRecords() {
-                var inputItem = data.records
-                $(inputItem).each(function(index, val) {
-                    if (val.fields.OrderNum == orderNum) {
-                        duplicate = true;
-                        return false; // break
+    function compareRecords() {
+    $('.item-row').each(function(){
+        var rowNum = $('.order-number',this).data('order-number');
+        if (rowNum == orderNum) {
+            duplicate = true;
+            return false; // break
+        }
+    });   
+    if (duplicate == true) {
+        dupePop();
+        console.log('we have a dupe');
+        return true;
+    }
+    }
+    if (compareRecords() != true) {
+        console.log('lets make a new order then');
+    registerOrder(orderNum, currentTime); // CREATES THE ORDER
+    }
+}
+
+
+
+
+function getDataAndBuild() {
+    var unique_values = {};
+    var list_of_values = [];
+
+   allOrders.eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+
+        records.forEach(function (record) {
+            var rowItem = '<div class="item-row flex-row row"><div data-order-number="' + record.fields.OrderNum + '" class="order-number fifty">' + record.fields.OrderNum + '</div><div data-order-time="' + record.fields.OrderDate + '" class="order-time fifty ">' + record.fields.OrderDate + '</div>';
+            if (!unique_values[record.fields.OrderNum]) {
+                unique_values[record.fields.OrderNum] = true;
+                list_of_values.push(record.OrderNum);
+                $(rowItem).appendTo('#currentOrders');
+                goToBottom();
+            } else {
+                console.log('there is a dupe');
+                // $(rowItem).addClass('duplicate-value').appendTo('#currentOrders');
+                base('Orders').destroy(record.id, function (err, deletedRecord) {
+                    if (err) {
+                        console.error(err);
+                        return;
                     }
+                    console.log('Deleted record', deletedRecord.id);
                 });
-                if (duplicate == true) {
+            }
 
-                    dupePop();
-                    return true
-                }
-            }
-            //console.log('compareRecords is ',compareRecords());
-            if (compareRecords() != true) {
-                registerOrder(orderNum, currentTime); // CREATES THE ORDER
-            }
-        },
-        error: function() {
-            console.log('Error occured fetching data');
+
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+
+    }, function done(err) {
+        if (err) {
+            console.error(err);
+            return;
         }
     });
+
 }
 
-function getDataAndBuild() { // FUNCTION TO POPULATE THE PAGE WITH CURRENT ORDERS AND DELETES ANY DUPLICATES FROM THE SYSTEM
-    $.ajax({
-        type: "GET",
-        url: jsonUrl,
-        success: function(data) {
-            console.log(data);
-            var inputItem = data.records
-            //console.log(data.records);
-            var unique_values = {};
-            var list_of_values = [];
-            $.each(inputItem, function(index, val) {
-                var rowItem = '<div class="item-row flex-row row"><div data-order-number="' + val.fields.OrderNum + '" class="order-number fifty">' + val.fields.OrderNum + '</div><div data-order-time="' + val.fields.OrderDate + '" class="order-time fifty ">' + val.fields.OrderDate + '</div>'
-                //console.log('value is',val.fields);
-                //console.log(rowItem);
-                if (!unique_values[val.fields.OrderNum]) {
-                    unique_values[val.fields.OrderNum] = true;
-                    list_of_values.push(val.OrderNum);
-                    $(rowItem).appendTo('#currentOrders');
-                    goToBottom();
-                } else {
-                    // console.log('there is a dupe');
-                    // $(rowItem).addClass('duplicate-value').appendTo('#currentOrders');
-                    base('Orders').destroy(val.id, function(err, deletedRecord) {
-                        if (err) {
-                            console.error(err);
-                            return;
-                        }
-                        console.log('Deleted record', deletedRecord.id);
-                    });
-                }
-            });
-        },
-        error: function() {
-            console.log('Error occured checking limit');
-        }
-    });
-}
+
 getDataAndBuild(); // POPULATE THE PAGE
 
 // modal instead of alert
@@ -132,21 +138,21 @@ var modal = document.getElementById('myModal');
 
 function dupePop() {
     modal.style.display = "block";
-};
+}
 
 function goToBottom() {
     var findCurrentOrder = $('#currentOrders');
     findCurrentOrder.scrollTop(findCurrentOrder.prop("scrollHeight"));
-};
+}
 
 function exportData() { // generate csv
     var csvContent = 'data:text/csv;charset=utf-8,';
-    $('#currentOrders .item-row').each(function(orderInfo, val) {
+    $('#currentOrders .item-row').each(function (orderInfo, val) {
         var orderNum = $('.order-number', this).data('order-number');
         var orderTime = $('.order-time', this).data('order-time');
-        var arrayInfo = orderNum + ',' + orderTime
+        var arrayInfo = orderNum + ',' + orderTime;
         console.log(orderNum + ',' + orderTime);
-        dataString = orderNum + ',' + orderTime
+        dataString = orderNum + ',' + orderTime;
         //console.log(orderInfo, val);
         csvContent += dataString + "\n";
 
@@ -157,7 +163,7 @@ function exportData() { // generate csv
     $('#exportBtn').attr('download', 'ofraOrders' + currentDay + '.csv').attr('href', encodedUri);
     //window.open(encodedUri);
 }
-$('#exportBtn').click(function() {
+$('#exportBtn').click(function () {
     exportData();
 });
 
@@ -166,10 +172,10 @@ function deleteAllOrders() {
         $.ajax({
             type: "GET",
             url: jsonUrl,
-            success: function(data) {
-                var inputItem = data.records
-                $.each(inputItem, function(index, val) {
-                    base('Orders').destroy(val.id, function(err, deletedRecord) {
+            success: function (data) {
+                var inputItem = data.records;
+                $.each(inputItem, function (index, val) {
+                    base('Orders').destroy(val.id, function (err, deletedRecord) {
                         if (err) {
                             console.error(err);
                             return;
@@ -184,6 +190,13 @@ function deleteAllOrders() {
         //console.log('delete canceled');
     }
 }
-$('#deleteAll').click(function() {
+$('#deleteAll').click(function () {
     deleteAllOrders();
+});
+
+
+// HIDE POPUP
+
+$('.remove-btn ').click(function(){
+    $('#myModal').fadeOut(500);
 });
